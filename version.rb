778bdb6ad68decdc6ee2a0bf7006ef2afe35e18a
winteen <cy_27@126.com>
2014-06-10 02:42:08 +0800

    Update version.rb

--- version.rb
+++ version.rb
@@ -1,3 +1,6 @@
+# Rails是一个Model对象。这里定义了一个gem_version方法。
+# gem_version方法返回了一个关于Version的描述对象。
+# 这里用一个Version Model对象来封装了版本相关的信息: major, minor, tiny, pre
 module Rails
   # Returns the version of the currently loaded Rails as a <tt>Gem::Version</tt>
   def self.gem_version
